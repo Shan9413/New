af44f492a1c9b479260c31ffbc6fbc1e7207242f
Rishav Dev Kumar <69079321+rishavdevkr@users.noreply.github.com>
2023-11-26 15:14:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,11 +27,11 @@ DEF_FORMAT = "480"
 from dotenv import load_dotenv
 load_dotenv()
 os.makedirs("./downloads", exist_ok=True)
-API_ID = 27039595
-API_HASH = "aabe9b61bbdb73ea4b35fc4faa880621"
-BOT_TOKEN = "6614216476:AAFDybAx7C27HAjo2AVPWiwxwwM_i2RMzvk"
-AUTH_USERS = 6387516803,
-sudo_users = [6387516803]
+API_ID = 22779671
+API_HASH = "125d8d88b77309dc3b154cbbfc2dacb2"
+BOT_TOKEN = "6152562853:AAGimPmtvHjqcE8em9iDMH-QAjkM8133P0c"
+AUTH_USERS = 1713924419
+sudo_users = [1713924419]
 bot = Client(
     "bot",
     bot_token=BOT_TOKEN,
